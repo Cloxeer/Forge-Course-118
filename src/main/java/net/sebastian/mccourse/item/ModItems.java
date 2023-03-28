130e27e9ae5b697bca5a213a74d414b4bfaf2ddb
Cloxeer <SSalgadoPhx@gmail.com>
2023-03-27 19:52:29 -0600

    Fences, Gates, Walls

--- src/main/java/net/sebastian/mccourse/item/ModItems.java
+++ src/main/java/net/sebastian/mccourse/item/ModItems.java
@@ -5,8 +5,7 @@ import net.minecraft.world.effect.MobEffectInstance;
 import net.minecraft.world.effect.MobEffects;
 import net.minecraft.world.food.FoodProperties;
 import net.minecraft.world.food.Foods;
-import net.minecraft.world.item.CreativeModeTab;
-import net.minecraft.world.item.Item;
+import net.minecraft.world.item.*;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
@@ -48,6 +47,21 @@ public class ModItems {
 
     public static final RegistryObject<Item> TURNIP = ITEMS.register("turnip",
             () -> new CoalSilverItem(new Item.Properties().tab(ModCreativeModeTab.COURSE_TAB).food(ModFoods.TURNIP)));
+
+    public static final RegistryObject<Item> COBALT_SWORD = ITEMS.register("cobalt_sword",
+            () -> new SwordItem(ModTiers.COBALT,  2 , 3f, new Item.Properties().tab(ModCreativeModeTab.COURSE_TAB)));
+
+    public static final RegistryObject<Item> COBALT_PICKAXE = ITEMS.register("cobalt_pickaxe",
+            () -> new PickaxeItem(ModTiers.COBALT,  2 , 3f, new Item.Properties().tab(ModCreativeModeTab.COURSE_TAB)));
+
+    public static final RegistryObject<Item> COBALT_AXE = ITEMS.register("cobalt_axe",
+            () -> new AxeItem(ModTiers.COBALT,  2 , 3f, new Item.Properties().tab(ModCreativeModeTab.COURSE_TAB)));
+
+    public static final RegistryObject<Item> COBALT_SHOVEL = ITEMS.register("cobalt_shovel",
+            () -> new ShovelItem(ModTiers.COBALT,  2 , 3f, new Item.Properties().tab(ModCreativeModeTab.COURSE_TAB)));
+
+    public static final RegistryObject<Item> COBALT_HOE = ITEMS.register("cobalt_hoe",
+            () -> new HoeItem(ModTiers.COBALT,  2 , 3f, new Item.Properties().tab(ModCreativeModeTab.COURSE_TAB)));
 
 
 
---
+++ src/main/java/net/sebastian/mccourse/item/ModTiers.java
@@ -0,0 +1,14 @@
+package net.sebastian.mccourse.item;
+
+import net.minecraft.tags.BlockTags;
+import net.minecraft.world.item.crafting.Ingredient;
+import net.minecraftforge.common.ForgeTier;
+
+public class ModTiers {
+
+    public static final ForgeTier COBALT = new ForgeTier(1, 1500, 1f, 4f, 10, BlockTags.NEEDS_IRON_TOOL,
+            () -> Ingredient.of(ModItems.COBALT_INGOT.get()));
+
+
+
+}
